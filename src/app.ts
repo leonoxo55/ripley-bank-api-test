a1f9e28c2ba9cd7694084a1d35f42d5d1899bc61
Alexsander <alex55.zuniga@gmail.com>
2021-10-29 18:13:47 -0300

    Add missing routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import bankRoutes from './routes/bank';
+import authRoutes from './routes/auth';
 
 const app: Application = express();
 
@@ -11,5 +12,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 app.use('/api/bank', bankRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
